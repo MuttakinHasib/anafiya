077ffb7988d01059983f2e3b7161c7f4cf8a7a63
MuttakinHasib <hasibmolla28@gmail.com>
2021-04-16 18:00:19 +0600

    prepare for deployment :rocket:

--- client/src/screens/NotFoundScreen.js
+++ client/src/screens/NotFoundScreen.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const NotFoundScreen = () => {
   return (
-    <div className='flex items-center justify-center'>
-      <h2 className='tex-4xl text-gray-700'>404</h2>
+    <div className='flex flex-col items-center justify-center text-center'>
+      <h2 className='text-5xl text-gray-700'>404</h2>
       <h4 className='text-lg text-gray-600'>Page Not Found</h4>
-      <p className='mt-8 prose'>
+      <p className='my-8 prose font-light'>
         The link you clicked may be broken or the page may have been removed or
         renamed.
       </p>
       <Link to='/' className='text-blue-400 underline'>
-        Go To Home
+        Go Back Home
       </Link>
     </div>
   );

0d00be65644a6c95a223446882964c14c3c8706c
MuttakinHasib <hasibmolla28@gmail.com>
2021-04-16 15:14:23 +0600

    prepare for deployment :rocket:

--- server/index.js
+++ server/index.js
@@ -26,14 +26,15 @@ app.use(cors());
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV !== 'production') {
-  app.use(morgan('dev'));
-  9;
-} else {
+if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
+}
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'));
+  9;
 }
 
 app.use(express.json());

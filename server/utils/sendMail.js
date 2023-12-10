2f92aacb8ce0b6ce0fd5b7f0eead137e75a7f297
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-12-11 03:08:08 +0600

    feat: 🛠️  fix mailer

--- server/utils/sendMail.js
+++ server/utils/sendMail.js
@@ -1,45 +1,22 @@
-import nodemailer from 'nodemailer';
-import { google } from 'googleapis';
-import { orderTables } from './htmlContent.js';
-const { OAuth2 } = google.auth;
-const OAUTH_PLAYGROUND = 'https://developers.google.com/oauthplayground';
+import nodemailer from "nodemailer";
 
-const {
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_REFRESH_TOKEN,
-  ADMIN_EMAIL,
-} = process.env;
+import { orderTables } from "./htmlContent.js";
 
-const oauth2Client = new OAuth2(
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_REFRESH_TOKEN,
-  OAUTH_PLAYGROUND
-);
+const { ADMIN_EMAIL, EMAIL_APP_PASSWORD } = process.env;
 
 export const sendActivationEmail = (to, url) => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
   const mailOptions = {
     from: `Anafiya < ${ADMIN_EMAIL}>`,
     to,
-    subject: 'Anafiya || Account Activation Link',
+    subject: "Anafiya || Account Activation Link",
     html: `
     <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
       <h2 style="text-align: center; text-transform: uppercase;color: #7C3BED;">Welcome to Anafiya Islamic Online Shop</h2>
@@ -58,25 +35,16 @@ export const sendActivationEmail = (to, url) => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
 
-export const sendOrderCreateEmail = order => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
+export const sendOrderCreateEmail = (order) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
@@ -84,8 +52,7 @@ export const sendOrderCreateEmail = order => {
     from: `Anafiya < ${ADMIN_EMAIL}>`,
     to: `${order?.shippingAddress?.name} < ${order?.shippingAddress?.email}>`,
     subject: `Anafiya - Order No# ${order._id}`,
-    text:
-      'Thank you for your interest in Proshop products. Your order has been received and will be processed once payment has been confirmed.', // plain text body
+    text: "Thank you for your interest in Proshop products. Your order has been received and will be processed once payment has been confirmed.", // plain text body
     html: `
       <div style='margin-bottom: 10px;'>
         <h4>Thank you for your interest in Proshop products. . Your order has been received and will be processed once payment has been confirmed.</h4>
@@ -97,25 +64,16 @@ export const sendOrderCreateEmail = order => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     // res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
 
-export const sendOrderCreateEmailToAdmin = order => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
+export const sendOrderCreateEmailToAdmin = (order) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
@@ -137,25 +95,16 @@ export const sendOrderCreateEmailToAdmin = order => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     // res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
 
-export const sendOrderPaidEmail = order => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
+export const sendOrderPaidEmail = (order) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
@@ -179,25 +128,16 @@ export const sendOrderPaidEmail = order => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     // res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
 
-export const sendOrderPaidEmailToAdmin = order => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
+export const sendOrderPaidEmailToAdmin = (order) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
@@ -222,25 +162,16 @@ export const sendOrderPaidEmailToAdmin = order => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     // res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };
 
-export const sendOrderDeliveredEmail = order => {
-  oauth2Client.setCredentials({
-    refresh_token: GOOGLE_REFRESH_TOKEN,
-  });
-
-  const accessToken = oauth2Client.getAccessToken();
+export const sendOrderDeliveredEmail = (order) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      type: 'OAuth2',
       user: ADMIN_EMAIL,
-      clientId: GOOGLE_CLIENT_ID,
-      clientSecret: GOOGLE_CLIENT_SECRET,
-      refreshToken: GOOGLE_REFRESH_TOKEN,
-      accessToken,
+      pass: EMAIL_APP_PASSWORD,
     },
   });
 
@@ -248,7 +179,7 @@ export const sendOrderDeliveredEmail = order => {
     from: `Anafiya < ${ADMIN_EMAIL}>`,
     to: `${order?.shippingAddress?.name} < ${order?.shippingAddress?.email}>`,
     subject: `Anafiya || Order Delivered - Order No# ${order._id}`, // Subject line
-    text: 'Your order has been delivered.', // plain text body
+    text: "Your order has been delivered.", // plain text body
     html: `
       <div style='margin-bottom: 10px;'>
         <h2>Your order has been delivered at ${order.deliveredAt}</h2>
@@ -260,6 +191,6 @@ export const sendOrderDeliveredEmail = order => {
   const emailSend = transporter.sendMail(mailOptions);
   if (!emailSend) {
     // res.status(400);
-    throw new Error('Something went wrong');
+    throw new Error("Something went wrong");
   }
 };

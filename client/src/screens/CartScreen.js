bb77fb56ccb819271a6c0622abec860100e3fe51
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-12-11 02:47:53 +0600

    feat: 🛠️  ready to deploy

--- client/src/screens/CartScreen.js
+++ client/src/screens/CartScreen.js
@@ -1,11 +1,11 @@
-import { motion } from 'framer-motion';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
-import emptyCartImg from '../assets/empty-cart.svg';
-import { Meta } from '../components';
-import Loader from '../components/Loader';
-import { addToCart, removeCartItem } from '../redux/actions/cartActions';
+import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
+import emptyCartImg from "../assets/empty-cart.svg";
+import { Meta } from "../components";
+import Loader from "../components/Loader";
+import { addToCart, removeCartItem } from "../redux/actions/cartActions";
 
 const CartScreen = () => {
   const location = useLocation();
@@ -13,8 +13,10 @@ const CartScreen = () => {
   const params = useParams();
   const dispatch = useDispatch();
   const productId = params.id;
-  const quantity = location.search ? Number(location.search.split('=')[1]) : 1;
-  const { cartItems, loading: cartLoading } = useSelector(state => state.cart);
+  const quantity = location.search ? Number(location.search.split("=")[1]) : 1;
+  const { cartItems, loading: cartLoading } = useSelector(
+    (state) => state.cart
+  );
 
   useEffect(() => {
     if (productId) {
@@ -39,21 +41,21 @@ const CartScreen = () => {
     <motion.div
       exit={{ opacity: 0 }}
       variants={stagger}
-      initial='hidden'
-      animate='visible'
-      className='space-y-10'
+      initial="hidden"
+      animate="visible"
+      className="space-y-10"
     >
-      <Meta title='Your shopping cart list' />
+      <Meta title="Your shopping cart list" />
       <Link
-        to='/'
-        className='py-3 px-5 bg-gray-100 rounded-md hover:bg-gray-200 transition-colors duration-300'
+        to="/"
+        className="py-3 px-5 bg-gray-100 rounded-md hover:bg-gray-200 transition-colors duration-300"
       >
         Go Home
       </Link>
-      <div className='flex justify-center items-center mt-20'>
-        <img src={emptyCartImg} width='250' height='250' alt='' />
+      <div className="flex justify-center items-center mt-20">
+        <img src={emptyCartImg} width="250" height="250" alt="" />
       </div>
-      <h1 className='text-center text-2xl lg:text-4xl text-gray-700'>
+      <h1 className="text-center text-2xl lg:text-4xl text-gray-700">
         Your Shopping Cart is Empty
       </h1>
     </motion.div>
@@ -61,44 +63,48 @@ const CartScreen = () => {
     <motion.div
       exit={{ opacity: 0, y: 5 }}
       variants={stagger}
-      initial='hidden'
-      animate='visible'
+      initial="hidden"
+      animate="visible"
     >
-      <Meta title='Your shopping cart list' />
+      <Meta title="Your shopping cart list" />
       {cartLoading && <Loader />}
-      <h2 className='text-4xl text-gray-700 mb-10'>Your Shopping Cart</h2>
-      <div className='grid gap-12 lg:grid-cols-3 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-full mx-auto'>
+      <h2 className="text-4xl text-gray-700 mb-10">Your Shopping Cart</h2>
+      <div className="grid gap-12 lg:grid-cols-3 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-full mx-auto">
         <motion.div
           exit={{ opacity: 0, y: 5 }}
           variants={stagger}
-          initial='hidden'
-          animate='visible'
-          className='lg:col-span-2 overflow-auto divide-y-2'
+          initial="hidden"
+          animate="visible"
+          className="lg:col-span-2 overflow-auto divide-y-2"
         >
-          {cartItems?.map(item => (
+          {cartItems?.map((item) => (
             <div
               key={item.product}
-              className='flex md:flex-row flex-col md:justify-between md:items-center md:space-x-8 space-y-3 py-3 pb-4'
+              className="flex md:flex-row flex-col md:justify-between md:items-center md:space-x-8 space-y-3 py-3 pb-4"
             >
-              <img className='w-2/3 mx-auto md:w-20' src={item?.image} alt='' />
+              <img
+                className="w-2/3 mx-auto md:w-20"
+                src={item?.image[0]}
+                alt=""
+              />
               <Link
                 to={`/product/${item?.product}`}
-                className='text-lg text-gray-700 hover:underline flex-1'
+                className="text-lg text-gray-700 hover:underline flex-1"
               >
                 {item?.name}
               </Link>
-              <div className='lg:w-32 lg:text-center'>${item?.price}</div>
+              <div className="lg:w-32 lg:text-center">${item?.price}</div>
               {/* <div className='w-full'> */}
               <select
-                className='w-1/2 md:w-20 lg:w-24 tex-sm h-9 border-none bg-gray-100 rounded-md focus:ring-purple-100'
-                name='quantity'
-                id='quantity'
+                className="w-1/2 md:w-20 lg:w-24 tex-sm h-9 border-none bg-gray-100 rounded-md focus:ring-purple-100"
+                name="quantity"
+                id="quantity"
                 value={item?.quantity}
-                onChange={e =>
+                onChange={(e) =>
                   dispatch(addToCart(item?.product, Number(e.target.value)))
                 }
               >
-                {[...Array(item?.countInStock).keys()].map(x => (
+                {[...Array(item?.countInStock).keys()].map((x) => (
                   <option value={x + 1} key={x + 1}>
                     {x + 1}
                   </option>
@@ -106,39 +112,39 @@ const CartScreen = () => {
               </select>
               {/* </div> */}
               <button
-                className='w-14 lg:w-16 bg-red-500 hover:bg-red-600 transition-colors duration-300 py-1 rounded-md text-white'
+                className="w-14 lg:w-16 bg-red-500 hover:bg-red-600 transition-colors duration-300 py-1 rounded-md text-white"
                 onClick={() => dispatch(removeCartItem(item?.product))}
               >
                 <svg
-                  className='w-5 lg:w-6 mx-auto'
-                  xmlns='http://www.w3.org/2000/svg'
-                  fill='none'
-                  viewBox='0 0 24 24'
-                  stroke='currentColor'
+                  className="w-5 lg:w-6 mx-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
                 >
                   <path
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                     strokeWidth={2}
-                    d='M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16'
+                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
                   />
                 </svg>
               </button>
             </div>
           ))}
         </motion.div>
-        <div className='lg:col-span-1'>
-          <div className=' border-2 border-dashed p-5 md:p-8 rounded-md'>
-            <h2 className='text-xl text-gray-700 font-semibold mb-5'>
+        <div className="lg:col-span-1">
+          <div className=" border-2 border-dashed p-5 md:p-8 rounded-md">
+            <h2 className="text-xl text-gray-700 font-semibold mb-5">
               Order Summary
             </h2>
-            <div className='flex justify-between items-center border-b-2 border-dashed pb-5'>
-              <h4 className='text-md text-gray-600'>
+            <div className="flex justify-between items-center border-b-2 border-dashed pb-5">
+              <h4 className="text-md text-gray-600">
                 Sub-total (
                 {cartItems?.reduce((acc, item) => acc + item.quantity, 0)})
                 Items
               </h4>
-              <span className='text-base text-gray-600'>
+              <span className="text-base text-gray-600">
                 $
                 {cartItems
                   ?.reduce((acc, item) => acc + item.quantity * item.price, 0)
@@ -147,10 +153,10 @@ const CartScreen = () => {
             </div>
             <button
               className={`${
-                cartItems?.length === 0 && 'opacity-50 pointer-events-none'
+                cartItems?.length === 0 && "opacity-50 pointer-events-none"
               } w-full mt-5 bg-purple-900 focus:outline-none focus:ring-4 focus:ring-purple-200 text-white px-5 py-2 transition-shadow duration-300`}
               disabled={cartItems?.length === 0}
-              onClick={() => navigate('/shipping')}
+              onClick={() => navigate("/shipping")}
             >
               Proceed to checkout
             </button>
--- client/src/screens/PlaceOrderScreen.js
+++ client/src/screens/PlaceOrderScreen.js
@@ -1,26 +1,26 @@
-import { motion } from 'framer-motion';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Link, useNavigate } from 'react-router-dom';
-import { CheckoutSteps, Meta } from '../components';
-import Loader from '../components/Loader';
-import { cartReset } from '../redux/actions/cartActions';
-import { createOrder } from '../redux/actions/orderActions';
+import { motion } from "framer-motion";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import { CheckoutSteps, Meta } from "../components";
+import Loader from "../components/Loader";
+import { cartReset } from "../redux/actions/cartActions";
+import { createOrder } from "../redux/actions/orderActions";
 import {
   ORDER_CREATE_RESET,
   ORDER_PAY_RESET,
   STRIPE_PAYMENT_RESET,
-} from '../redux/actions/types';
+} from "../redux/actions/types";
 
 const PlaceOrderScreen = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { user } = useSelector(state => state.userLogin);
-  const cart = useSelector(state => state.cart);
-  const { order, success, loading } = useSelector(state => state.orderCreate);
+  const { user } = useSelector((state) => state.userLogin);
+  const cart = useSelector((state) => state.cart);
+  const { order, success, loading } = useSelector((state) => state.orderCreate);
 
-  const addDecimals = num => (Math.round(num * 100) / 100).toFixed(2);
+  const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2);
 
   cart.itemsPrice = addDecimals(
     cart.cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
@@ -36,20 +36,20 @@ const PlaceOrderScreen = () => {
 
   useEffect(() => {
     if (!user) {
-      navigate('/login');
+      navigate("/login");
     } else {
       dispatch({ type: ORDER_CREATE_RESET });
       dispatch({ type: ORDER_PAY_RESET });
       dispatch({ type: STRIPE_PAYMENT_RESET });
       if (success) {
         dispatch(cartReset());
-        if (order?.paymentMethod === 'cashOnDelivery') {
+        if (order?.paymentMethod === "cashOnDelivery") {
           navigate(`/order/${order._id}/success`);
         } else {
           navigate(`/order/${order._id}`);
         }
       } else if (cart?.cartItems.length === 0) {
-        navigate('/');
+        navigate("/");
       }
     }
     // eslint-disable-next-line
@@ -58,7 +58,10 @@ const PlaceOrderScreen = () => {
   const placeOrderHandler = () => {
     dispatch(
       createOrder({
-        orderItems: cart.cartItems,
+        orderItems: cart.cartItems.map((item) => ({
+          ...item,
+          image: item.image[0],
+        })),
         paymentMethod: cart.paymentMethod,
         shippingAddress: cart.shippingAddress,
         shippingPrice: cart.shippingPrice,
@@ -86,38 +89,38 @@ const PlaceOrderScreen = () => {
     <motion.div
       exit={{ opacity: 0, y: 5 }}
       variants={stagger}
-      initial='hidden'
-      animate='visible'
+      initial="hidden"
+      animate="visible"
     >
-      <Meta title='Place order' />
+      <Meta title="Place order" />
       {loading && <Loader />}
       <CheckoutSteps step1 step2 step3 />
-      <div className='grid gap-12 lg:grid-cols-3 mt-12 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-full mx-auto'>
-        <div className='lg:col-span-2 space-y-7'>
+      <div className="grid gap-12 lg:grid-cols-3 mt-12 max-w-sm sm:max-w-md md:max-w-xl lg:max-w-full mx-auto">
+        <div className="lg:col-span-2 space-y-7">
           <div>
-            <h2 className='text-2xl text-gray-600 mb-5'>Ordered Items</h2>
+            <h2 className="text-2xl text-gray-600 mb-5">Ordered Items</h2>
             <motion.div
               exit={{ opacity: 0, y: 5 }}
               variants={stagger}
-              initial='hidden'
-              animate='visible'
-              className='border-2 border-gray-200 rounded-md px-8 py-5 divide-y-2 space-y-2'
+              initial="hidden"
+              animate="visible"
+              className="border-2 border-gray-200 rounded-md px-8 py-5 divide-y-2 space-y-2"
             >
-              {cart?.cartItems.map(item => (
+              {cart?.cartItems.map((item) => (
                 <div
-                  className='flex md:items-center flex-col md:flex-row justify-between'
+                  className="flex md:items-center flex-col md:flex-row justify-between"
                   key={item?.product}
                 >
-                  <div className='flex items-center'>
-                    <img src={item?.image} alt='' className='w-20 mr-8' />
+                  <div className="flex items-center">
+                    <img src={item?.image[0]} alt="" className="w-20 mr-8" />
                     <Link
                       to={`/product/${item?.product}`}
-                      className='text-base md:text-lg text-gray-600 hover:underline'
+                      className="text-base md:text-lg text-gray-600 hover:underline"
                     >
                       {item?.name}
                     </Link>
                   </div>
-                  <p className='prose'>
+                  <p className="prose">
                     {item?.quantity} x ${item?.price} = $
                     {Number(item?.quantity * item?.price).toFixed(2)}
                   </p>
@@ -126,27 +129,27 @@ const PlaceOrderScreen = () => {
             </motion.div>
           </div>
           <div>
-            <h2 className='text-2xl text-gray-600 mb-5'>Payment Method</h2>
-            <div className='border-2 border-gray-200 rounded-md p-8'>
-              <h2 className='text-lg capitalize'>{cart?.paymentMethod}</h2>
+            <h2 className="text-2xl text-gray-600 mb-5">Payment Method</h2>
+            <div className="border-2 border-gray-200 rounded-md p-8">
+              <h2 className="text-lg capitalize">{cart?.paymentMethod}</h2>
             </div>
           </div>
           <div>
-            <h2 className='text-2xl text-gray-600 mb-5'>Shipping Details</h2>
-            <div className='border-2 border-gray-200 hover:bg-blue-400 hover:bg-opacity-5 rounded-md p-8'>
-              <h4 className='text-lg text-gray-700'>
+            <h2 className="text-2xl text-gray-600 mb-5">Shipping Details</h2>
+            <div className="border-2 border-gray-200 hover:bg-blue-400 hover:bg-opacity-5 rounded-md p-8">
+              <h4 className="text-lg text-gray-700">
                 {cart?.shippingAddress?.name}
               </h4>
-              <div className='text-gray-500 font-light'>
+              <div className="text-gray-500 font-light">
                 <a
-                  className='hover:underline text-'
+                  className="hover:underline text-"
                   href={`mailto:${user?.email}`}
                 >
                   {user?.email}
                 </a>
                 <p>{cart?.shippingAddress?.phone}</p>
                 <p>
-                  {cart?.shippingAddress?.address} {cart?.shippingAddress?.city}{' '}
+                  {cart?.shippingAddress?.address} {cart?.shippingAddress?.city}{" "}
                   - {cart?.shippingAddress?.postcode}
                 </p>
                 <p>{cart?.shippingAddress?.country}</p>
@@ -154,14 +157,14 @@ const PlaceOrderScreen = () => {
             </div>
           </div>
         </div>
-        <div className='lg:col-span-1'>
-          <div className='border-2 border-dashed p-8 rounded-md'>
-            <h2 className='text-xl text-gray-700 font-semibold mb-5'>
+        <div className="lg:col-span-1">
+          <div className="border-2 border-dashed p-8 rounded-md">
+            <h2 className="text-xl text-gray-700 font-semibold mb-5">
               Order Summary
             </h2>
-            <div className='space-y-5'>
-              <div className='flex justify-between items-center'>
-                <h4 className='text-md text-gray-600'>
+            <div className="space-y-5">
+              <div className="flex justify-between items-center">
+                <h4 className="text-md text-gray-600">
                   Sub-total (
                   {cart?.cartItems?.reduce(
                     (acc, item) => acc + item.quantity,
@@ -169,34 +172,34 @@ const PlaceOrderScreen = () => {
                   )}
                   ) Items
                 </h4>
-                <span className='text-base text-gray-600'>
+                <span className="text-base text-gray-600">
                   ${cart?.itemsPrice}
                 </span>
               </div>
-              <div className='flex justify-between items-center'>
-                <h4 className='text-md text-gray-600'>Shipping Price</h4>
-                <span className='text-base text-gray-600'>
+              <div className="flex justify-between items-center">
+                <h4 className="text-md text-gray-600">Shipping Price</h4>
+                <span className="text-base text-gray-600">
                   ${cart?.shippingPrice}
                 </span>
               </div>
-              <div className='flex justify-between items-center'>
-                <h4 className='text-md text-gray-600'>Tax</h4>
-                <span className='text-base text-gray-600'>
+              <div className="flex justify-between items-center">
+                <h4 className="text-md text-gray-600">Tax</h4>
+                <span className="text-base text-gray-600">
                   ${cart?.taxPrice}
                 </span>
               </div>
-              <div className='flex justify-between items-center border-b-2 border-dashed pb-5'>
-                <h4 className='text-lg font-semibold text-gray-700'>Total</h4>
+              <div className="flex justify-between items-center border-b-2 border-dashed pb-5">
+                <h4 className="text-lg font-semibold text-gray-700">Total</h4>
                 <span
-                  className='text-lg font-semibold'
-                  style={{ color: '#f36' }}
+                  className="text-lg font-semibold"
+                  style={{ color: "#f36" }}
                 >
                   ${cart?.totalPrice}
                 </span>
               </div>
             </div>
             <button
-              className='w-full mt-5 bg-purple-900 focus:outline-none focus:ring-4 focus:ring-purple-200 text-white px-5 py-2 transition-shadow duration-300'
+              className="w-full mt-5 bg-purple-900 focus:outline-none focus:ring-4 focus:ring-purple-200 text-white px-5 py-2 transition-shadow duration-300"
               onClick={placeOrderHandler}
             >
               Place Order
